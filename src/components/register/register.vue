d6be3f4cfb90f8513e7db778059c3f6643d3cbf8
Justin Hays <jhays478@gmail.com>
2020-03-16 07:10:42 -0500

    store gets populated with user and account information upon sign in and registration

--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -40,12 +40,15 @@ export default {
 
         try {
           const response = await this.$http.POST('api/AppUsers/Register', this.getPayload());
-          // dispatch / commit response
+          this.$store.dispatch('signIn', response.data);
           
           alerts.success();
         }
         catch (error) {
-          alerts.error({ text: error.request.responseText });
+          this.isLoading = false;
+          const text = error && error.request ? error.request.responseText : 'An error occured while trying to sign you in. Please try again or contact support.'
+
+          alerts.error({ text: text });
         }
         finally {
           this.isLoading = false;
