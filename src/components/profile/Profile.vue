95ac543bfc06f0af8f9be9b75ab12a3469f2cb8c
Justin Hays <jhays478@gmail.com>
2020-03-20 08:57:05 -0500

    updating profile patch request

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -3,6 +3,7 @@
     <h2>Profile</h2>
     <v-text-field @input="updateProfile('firstName', $event)" :value="profile.data.firstName" label="First name" type="text" />
     <v-text-field @input="updateProfile('lastName', $event)" :value="profile.data.lastName" label="Last name" type="text" />
+    <v-text-field @input="updateProfile('email', $event)" :value="profile.data.email" label="Email" type="email" />
     <v-btn @click="save" :disabled="loading || profile.hasChanges" :loading="loading">Save Profile</v-btn>
   </v-container>
 </template>
@@ -24,19 +25,38 @@ export default {
     updateProfile(key, value) {
       this.$store.commit('updateProfile', { key, value });
     },
-    save() {
+    async save() {
       try {
         this.loading = true;
-        // TODO call Api with payload
+        
+        const response = await this.$http.PATCH(`api/AppUsers(${this.profile.data.id})`, this.getPayload());
         this.$store.dispatch('saveProfile');
       }
-      catch (ex) {
+      catch (error) {
+        const text = error && error.request ? error.request.responseText : 'An error occured while trying to sign you in. Please try again or contact support.'
 
+        alerts.error({ text: text });
       }
       finally {
         this.loading = false;
       }
-    }
+    },
+    getPayload() {
+      return [
+        {
+          name: 'firstName',
+          value: this.profile.data.firstName
+        },
+        {
+          name: 'lastName',
+          value: this.profile.data.lastName
+        },
+        {
+          name: 'email',
+          value: this.profile.data.email
+        }
+      ];
+    }    
   },
   mounted() {
     this.$store.commit('setProfile');
